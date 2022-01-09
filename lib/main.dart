6177400700e035c536df67fb4a8c92d53d9561cc
kazuyaseki <track613armory@gmail.com>
2022-01-10 02:18:07 +0900

    fix main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:write_only_twitter/src/Widgets/Button.dart';
 import 'package:write_only_twitter/src/screens/HomeScreen.dart';
 import 'package:write_only_twitter/src/screens/LoginScreen.dart';
-import 'package:write_only_twitter/src/theme/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Write Only Twitter',
       theme: ThemeData(
         scaffoldBackgroundColor: const Color(0xFFFFFF)
       ),
