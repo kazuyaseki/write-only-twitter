4ef68829c4e3e37ba8c093332c46a62c2a91fb44
kazuyaseki <track613armory@gmail.com>
2022-01-11 20:29:21 +0900

    added photofilter

--- lib/src/components/CreateTweetModal.dart
+++ lib/src/components/CreateTweetModal.dart
@@ -1,14 +1,73 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mix/mix.dart';
+import 'package:path/path.dart';
 import 'package:gap/gap.dart';
+import 'package:image/image.dart' as imageLib;
+import 'package:image_picker/image_picker.dart';
+import 'package:photofilters/filters/filters.dart';
+import 'package:photofilters/filters/preset_filters.dart';
+import 'package:photofilters/photofilters.dart';
 import 'package:write_only_twitter/src/components/Button.dart';
 import 'package:write_only_twitter/src/theme/colors.dart';
 import 'package:write_only_twitter/src/theme/typography.dart';
 
-class CreateTweetModal extends StatelessWidget {
-  const CreateTweetModal({Key? key}) : super(key: key);
+class CreateTweetModal extends StatefulWidget {
+  @override
+  _CreateTweetModalState createState() => new _CreateTweetModalState();
+}
+
+class _CreateTweetModalState extends State<CreateTweetModal> {
+  String fileName = "";
+  List<Filter> filters = presetFiltersList;
+  XFile? imageFile;
 
   @override
   Widget build(BuildContext context) {
+    Future getImage(context) async {
+      try {
+        print("hoge");
+        final ImagePicker _picker = ImagePicker();
+        imageFile = await _picker.pickImage(source: ImageSource.gallery);
+        print(imageFile);
+      } on PlatformException catch (e) {
+        print(e);
+      }
+      print(imageFile?.name);
+
+      if (imageFile == null) {
+        return;
+      }
+
+      fileName = basename(imageFile!.path);
+      Uint8List imageBytes = await imageFile!.readAsBytes();
+      var image = imageLib.decodeImage(imageBytes);
+      image = imageLib.copyResize(image!, width: 600);
+
+      Map imagefile = await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PhotoFilterSelector(
+            title: const Text("Photo Filter Example"),
+            image: image!,
+            filters: presetFiltersList,
+            filename: fileName,
+            loader: const Center(child: CircularProgressIndicator()),
+            fit: BoxFit.contain,
+          ),
+        ),
+      );
+      if (imagefile != null && imagefile.containsKey('image_filtered')) {
+        setState(() {
+          imageFile = imagefile['image_filtered'];
+        });
+        print(imageFile!.path);
+      }
+    }
+
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
@@ -47,24 +106,27 @@ class CreateTweetModal extends StatelessWidget {
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: const [
-                      Icon(
-                        Icons.image_outlined,
-                        color: PrimaryTwitterBlue,
-                        size: 24.0,
-                        semanticLabel:
-                            'Text to announce in accessibility modes',
-                      ),
-                      Gap(20),
-                      Icon(
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            getImage(context);
+                          },
+                          iconSize: 24,
+                          icon: const Icon(
+                            Icons.image_outlined,
+                            color: PrimaryTwitterBlue,
+                            semanticLabel: 'Add Images',
+                          )),
+                      const Gap(4),
+                      const Icon(
                         Icons.gif,
                         color: PrimaryTwitterBlue,
                         size: 24.0,
                         semanticLabel:
                             'Text to announce in accessibility modes',
                       ),
-                      Gap(20),
-                      Icon(
+                      const Gap(20),
+                      const Icon(
                         Icons.poll_outlined,
                         color: PrimaryTwitterBlue,
                         size: 24.0,
